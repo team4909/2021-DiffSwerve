c7539176746f57a6187c8cb50e497bbdf9ee8b9e
Derrell Lipman <derrell.lipman@unwireduniverse.com>
2021-03-26 10:09:46 -0400

    More places where gear ratios were renamed

--- src/main/java/frc/peyton/PeytonSwerveModule.java
+++ src/main/java/frc/peyton/PeytonSwerveModule.java
@@ -12,27 +12,39 @@
 
 package frc.peyton;
 
+import frc.bionic.swerve.*;
+
 public class PeytonSwerveModule extends frc.bionic.swerve.SwerveModule
 {
   // Gear ratio of first two pairs of gears.
   // Yaw does not depend on the third pair.
-  private static final double       GEAR_RATIO_12  =  (10.0/80.0) * (34.0/90.0);
+  private static final double       GEAR_RATIO_YAW          =  (10.0/80.0) * (34.0/90.0);
 
   // Gear ratio of all three pairs of gears.
   // Wheel speed depends on all three.
-  private static final double       GEAR_RATIO_123 =  (10.0/80.0) * (34.0/90.0) * (82.0/21.0);
+  private static final double       GEAR_RATIO_WHEEL_SPEED  =  (10.0/80.0) * (34.0/90.0) * (82.0/21.0);
 
   // Maximum yaw speed in RPM
-  private static final double       MAX_YAW_SPEED_RPM = 200;
+  private static final double       MAX_YAW_SPEED_RPM       = 200;
+
+  // The wheel diameter, in meters
+  private static final double      WHEEL_DIAMETER_METERS    = frc.bionic.Conversion.inchesToMeters(3.0);
 
   public PeytonSwerveModule(int pwmMotorChannelA, int pwmMotorChannelB,
                             int dioEncoderChannelA, int dioEncoderChannelB,
                             String name, String shuffleboardTabName)
   {
-    super(GEAR_RATIO_12, GEAR_RATIO_123, MAX_YAW_SPEED_RPM, 
-          pwmMotorChannelA, pwmMotorChannelB,
-          dioEncoderChannelA, dioEncoderChannelB,
-          frc.bionic.Conversion.inchesToMeters(3.0),
-          name, shuffleboardTabName);
+    super(name, shuffleboardTabName);
+
+    // Instantiate the two swerve motors our swerve module incorporates
+    IMotor motorA = new MotorCANSparkMaxNeo(pwmMotorChannelA, name + " A", shuffleboardTabName);
+    IMotor motorB = new MotorCANSparkMaxNeo(pwmMotorChannelB, name + " B", shuffleboardTabName);
+
+    // Instantiate the yaw encoder our swerve module incorporates
+    IYawEncoder encoder = new YawEncoderGrayhill63R128(dioEncoderChannelA, dioEncoderChannelB, 
+                                           name + " Grayhill", shuffleboardTabName);
+
+    initialize(GEAR_RATIO_YAW, GEAR_RATIO_WHEEL_SPEED, MAX_YAW_SPEED_RPM, 
+               motorA, motorB, encoder, WHEEL_DIAMETER_METERS);
   }
 }
