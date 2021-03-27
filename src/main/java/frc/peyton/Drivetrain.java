150eced12c7746ba704786f501845665bebe50c6
Blake <Techplex.Engineer@gmail.com>
2021-03-27 10:08:22 -0400

    single module working with joystick

---
+++ src/main/java/frc/peyton/Drivetrain.java
@@ -0,0 +1,96 @@
+/*
+ * Team 4909, Bionics
+ * Billerica Memorial High School
+ *
+ * Copyright:
+ *   2021 Bionics
+ *
+ * License:
+ *   MIT: https://opensource.org/licenses/MIT
+ *   See the LICENSE file in the project's top-level directory for details.
+ */
+
+package frc.peyton;
+
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.kinematics.SwerveDriveKinematics;
+
+public class Drivetrain
+{
+  public PeytonSwerveModule    swerveRF; // right front
+  public PeytonSwerveModule    swerveLF; // left front
+  public PeytonSwerveModule    swerveLR; // left rear
+  public PeytonSwerveModule    swerveRR; // right rear
+
+  private final double kHalfWheelBaseWidthInches;
+  private final double kHalfWheelBaseWidthMeters;
+  private final double kHalfWheelBaseLengthInches;
+  private final double kHalfWheelBaseLengthMeters;
+
+  private final double kMaxSpeed;
+  private final double kMaxAngularSpeed;
+
+  private final Translation2d m_frontLeftLocation;
+  private final Translation2d m_frontRightLocation;
+  private final Translation2d m_backLeftLocation;
+  private final Translation2d m_backRightLocation;
+
+  private final SwerveDriveKinematics m_kinematics;
+
+
+  public Drivetrain()
+  {
+    kHalfWheelBaseWidthInches  = 15.0;
+    kHalfWheelBaseWidthMeters  = frc.bionic.Conversion.inchesToMeters(kHalfWheelBaseWidthInches);
+
+    kHalfWheelBaseLengthInches = 15.0;
+    kHalfWheelBaseLengthMeters = frc.bionic.Conversion.inchesToMeters(kHalfWheelBaseLengthInches);
+  
+    kMaxSpeed = 3.0; // 3 meters per second
+    kMaxAngularSpeed = Math.PI; // 1/2 rotation per second
+  
+    m_frontLeftLocation  =
+      new Translation2d( kHalfWheelBaseWidthMeters,  kHalfWheelBaseLengthMeters);
+    m_frontRightLocation = 
+      new Translation2d( kHalfWheelBaseWidthMeters, -kHalfWheelBaseLengthMeters);
+    m_backLeftLocation   = 
+      new Translation2d(-kHalfWheelBaseWidthMeters,  kHalfWheelBaseLengthMeters);
+    m_backRightLocation  = 
+      new Translation2d(-kHalfWheelBaseWidthMeters, -kHalfWheelBaseLengthMeters);
+  
+    m_kinematics = new SwerveDriveKinematics(m_frontLeftLocation, m_frontRightLocation,
+                                             m_backLeftLocation, m_backRightLocation);
+
+    swerveRF = new PeytonSwerveModule(1, 2, 0, 1, "RF", "Peyton");
+    // swerveLF = new PeytonSwerveModule(3, 4, 2, 3, "LF", "Peyton");
+    // swerveLR = new PeytonSwerveModule(5, 6, 4, 5, "LR", "Peyton");
+    // swerveRR = new PeytonSwerveModule(7, 8, 6, 7, "RR", "Peyton");
+  }
+
+  public void periodic()
+  {
+    swerveRF.periodic();
+    // swerveLF.periodic();
+    // swerveLR.periodic();
+    // swerveRR.periodic();
+  }
+
+  /**
+   * Method to drive the robot using joystick info.
+   *
+   * @param xSpeed Speed of the robot in the x direction (forward).
+   * @param ySpeed Speed of the robot in the y direction (sideways).
+   * @param rot Angular rate of the robot.
+   */
+  public void drive(double xSpeed, double ySpeed, double rot)
+  {
+    var swerveModuleStates = m_kinematics.toSwerveModuleStates(new ChassisSpeeds(xSpeed, ySpeed, rot));
+    SwerveDriveKinematics.normalizeWheelSpeeds(swerveModuleStates, kMaxSpeed);
+    
+    swerveRF.setModuleState(swerveModuleStates[0]);
+    // swerveLF.setDesiredState(swerveModuleStates[1]);
+    // swerveLR.setDesiredState(swerveModuleStates[2]);
+    // swerveRR.setDesiredState(swerveModuleStates[3]);
+  }
+}
--- src/main/java/frc/peyton/Robot.java
+++ src/main/java/frc/peyton/Robot.java
@@ -12,37 +12,104 @@
 
 package frc.peyton;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SlewRateLimiter;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class Robot extends TimedRobot
-{
-  private PeytonSwerveModule    swerveRF; // right front
-  private PeytonSwerveModule    swerveLF; // left front
-  private PeytonSwerveModule    swerveLR; // left rear
-  private PeytonSwerveModule    swerveRR; // right rear
+public class Robot extends TimedRobot {
+  private final Joystick m_controller = new Joystick(0);
+  private final Drivetrain m_swerve = new Drivetrain();
 
-  public void robotInit()
-  {
-    swerveRF = new PeytonSwerveModule(1, 2, 0, 1, "RF", "Peyton");
-    // swerveLF = new PeytonSwerveModule(3, 4, 2, 3, "LF", "Peyton");
-    // swerveLR = new PeytonSwerveModule(5, 6, 4, 5, "LR", "Peyton");
-    // swerveRR = new PeytonSwerveModule(7, 8, 6, 7, "RR", "Peyton");
+  // Slew rate limiters to make joystick inputs more gentle; 1/3 sec from 0 to 1.
+  private final SlewRateLimiter m_xspeedLimiter = new SlewRateLimiter(3);
+  private final SlewRateLimiter m_yspeedLimiter = new SlewRateLimiter(3);
+  private final SlewRateLimiter m_rotLimiter = new SlewRateLimiter(3);
+
+  @Override
+  public void robotInit() {
+    // NetworkTable table = NetworkTableInstance.getDefault().getTable("MyTable");
+    // table.getEntry("")
+    SmartDashboard.putBoolean("Override", false);
+    SmartDashboard.putBoolean("ZERO", false);
   }
 
-  public void robotPeriodic()
-  {
-    swerveRF.periodic();
-    // swerveLF.periodic();
-    // swerveLR.periodic();
-    // swerveRR.periodic();
+  @Override
+  public void robotPeriodic() {
+    m_swerve.periodic();
   }
 
-  public void teleopInit()
-  {
-    // For debugging, reset all encoders to zero when beginning teleop
-    swerveRF.setZero();
-    // swerveLF.setZero();
-    // swerveLR.setZero();
-    // swerveRR.setZero();
+  @Override
+  public void teleopInit() {
+  }
+
+  // @Override
+  // public void autonomousPeriodic() {
+  //   driveWithJoystick(false);
+  //   m_swerve.updateOdometry();
+  // }
+
+  @Override
+  public void teleopPeriodic() {
+    driveWithJoystick(true);
+  }
+
+  private void driveWithJoystick(boolean fieldRelative) {
+    // Get the x speed. We are inverting this because Xbox controllers return
+    // negative values when we push forward.
+    double xSpeed = -m_controller.getY();
+        // -m_xspeedLimiter.calculate(m_controller.getY(GenericHID.Hand.kLeft))
+        //     * frc.robot.Drivetrain.kMaxSpeed;
+
+    // Get the y speed or sideways/strafe speed. We are inverting this because
+    // we want a positive value when we pull to the left. Xbox controllers
+    // return positive values when you pull to the right by default.
+    double ySpeed = -m_controller.getX();
+        // -m_yspeedLimiter.calculate(m_controller.getX(GenericHID.Hand.kLeft))
+        //     * frc.robot.Drivetrain.kMaxSpeed;
+
+    // Get the rate of angular rotation. We are inverting this because we want a
+    // positive value when we pull to the left (remember, CCW is positive in
+    // mathematics). Xbox controllers return positive values when you pull to
+    // the right by default.
+    double rot = -m_controller.getZ();
+        // -m_rotLimiter.calculate()
+        //     * frc.robot.Drivetrain.kMaxAngularSpeed;
+
+    if (Math.abs(rot) < .1) {
+      rot = 0;
+    }
+    if (Math.abs(xSpeed) < .1) {
+      xSpeed = 0;
+    }
+    if (Math.abs(ySpeed) < .1) {
+      ySpeed = 0;
+    }
+
+    
+
+    if (SmartDashboard.getBoolean("Override", false)) {
+      xSpeed = SmartDashboard.getNumber("xSpeed", 0);
+      ySpeed = SmartDashboard.getNumber("ySpeed", 0);
+      rot    = SmartDashboard.getNumber("rot",    0);
+    }
+
+    if (SmartDashboard.getBoolean("ZERO", false)) {
+      SmartDashboard.putBoolean("ZERO", false);
+      xSpeed = 0;
+      ySpeed = 0;
+      rot    = 0;
+    }
+
+    SmartDashboard.putNumber("xSpeed", xSpeed);
+    SmartDashboard.putNumber("ySpeed", ySpeed);
+    SmartDashboard.putNumber("rot",    rot);
+
+    m_swerve.drive(xSpeed, ySpeed, rot);
   }
 }
