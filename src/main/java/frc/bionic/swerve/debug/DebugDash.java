7080cf3892ea48a90ca90667ed2ac9c36459c421
Blake Bourque <techplex.engineer@gmail.com>
2021-05-28 14:59:47 -0400

    Decided TabEncoderSetup was not a good idea

--- src/main/java/frc/bionic/swerve/debug/DebugDash.java
+++ src/main/java/frc/bionic/swerve/debug/DebugDash.java
@@ -1,18 +1,6 @@
 package frc.bionic.swerve.debug;
 
-import java.util.Map;
-
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
-import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.bionic.swerve.AbstractDrivetrain;
-import frc.bionic.swerve.AbstractSwerveModule;
-import frc.bionic.swerve.IMotor;
-import frc.robot.Robot;
 
 public class DebugDash {
 
@@ -24,11 +12,13 @@ public class DebugDash {
 		motorTab = new TabMotor(drivetrain);
 		modTab = new TabModule(drivetrain);
 		dtTab = new TabDrivetrain(drivetrain);
+		// esTab = new TabEncoderSetup(drivetrain);
 	}
 
 	public void periodic() {
 		motorTab.periodic();
 		modTab.periodic();
 		dtTab.periodic();
+		// esTab.periodic();
 	}
 }
