ab5e3a4ae831ffe3ec346be6669e81d1868c803b
Dhvan <34456470+Dh-Van@users.noreply.github.com>
2021-06-15 12:40:04 -0400

    (Maybe) Fixed the problem we were having yesterday

--- src/main/java/frc/robot/subsystems/shooter/HoodSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooter/HoodSubsystem.java
@@ -1,24 +1,15 @@
 package frc.robot.subsystems.shooter;
 
-import javax.swing.text.Position;
-
 import com.revrobotics.CANEncoder;
-import com.revrobotics.CANError;
 import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.ControlType;
 import com.revrobotics.EncoderType;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.Spark;
-import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
-import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile.Constraints;
-import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.bionic.SlewRateLimiter;
 
 public class HoodSubsystem extends SubsystemBase{
 
@@ -27,23 +18,42 @@ public class HoodSubsystem extends SubsystemBase{
     CANPIDController hoodPID;
     CANEncoder hoodEncoder;
     double hoodPos;
-    SlewRateLimiter slewRateLimiter;
 
+    // Trapezoidal Profile Constraints, MaxVal and MaxAcell
     private final TrapezoidProfile.Constraints constraints =
         new TrapezoidProfile.Constraints(1.75, 0.75);
 
     private TrapezoidProfile.State goal = new TrapezoidProfile.State();
     private TrapezoidProfile.State setpoint = new TrapezoidProfile.State();
 
-    //There needs to be a 'd' at the end of the constants to explicitly mark it as a double! 
+    //There needs to be a 'd' at the end of the constants to explicitly mark it as a double
     // How many ticks are in one reovlution of a motor
     int TICKS_PER_REVOLUTION_MOTOR = 42;
     // How many rotations of a motor turns the gear
     double REVOLUTIONS_MOTOR_PER_REVOLUTION_OUTPUT_SHAFT = 16d;
     // How much the hood moves per rotation of the gear
-    double REVOLUTION_HOOD_PER_REVOLUTION_GEAR = 400d/69d;
+    double REVOLUTION_GEAR_PER_REVOLUTION_HOOD = 400d/69d;
     // How much the hood moves per degree
     double REVOLUTION_HOOD_PER_DEGREES = 1d/360d;
+    // Converts motor ticks to hood degrees
+    double MOTOR_TICKS_TO_HOOD_DEGREES =  TICKS_PER_REVOLUTION_MOTOR *
+                                          REVOLUTIONS_MOTOR_PER_REVOLUTION_OUTPUT_SHAFT *
+                                          REVOLUTION_GEAR_PER_REVOLUTION_HOOD *
+                                          REVOLUTION_HOOD_PER_DEGREES;   
+    
+    double HOOD_DEGREES_TO_MOTOR_TICKS =  REVOLUTION_HOOD_PER_DEGREES /
+                                          REVOLUTION_GEAR_PER_REVOLUTION_HOOD /
+                                          REVOLUTIONS_MOTOR_PER_REVOLUTION_OUTPUT_SHAFT /
+                                          TICKS_PER_REVOLUTION_MOTOR;
+
+    
+
+    // Max amount of degrees the hood is allowed to move
+    double MAX_DEGREES_HOOD = 35;
+    // Min amount of degrees the hood is allowed to move
+    double MIN_DEGREES_HOOD = 0;
+    // How much the hood is allowed to move per call
+    double HOOD_INCREMENT_DEGREES = 0.5;
 
     public HoodSubsystem(){
         hoodMotor = new CANSparkMax(12, MotorType.kBrushless);
@@ -53,14 +63,29 @@ public class HoodSubsystem extends SubsystemBase{
 
         // TODO: Encoder takes in Channel A, Channel B, and reverse Direction
         hoodEncoder = hoodMotor.getEncoder(EncoderType.kHallSensor, TICKS_PER_REVOLUTION_MOTOR);
-        // hoodMotor.setClosedLoopRampRate(1);
-        // hoodMotor.setOpenLoopRampRate(1);
-        hoodMotor.setSmartCurrentLimit(1);
+
+        {
         // Turns the position of the hood, so one "tick" of the motor is equal to one degree of the hood
+        /**
+         *  TODO I think we are using this method wrong 
+         *  documentation says that default is: REVOLUTIONS PER VOLT
+         *  https://www.revrobotics.com/content/sw/max/sw-docs/SPARK-MAX-Java-API-Offline.pdf
+        //  * */ 
         // hoodEncoder.setPositionConversionFactor(REVOLUTIONS_MOTOR_PER_REVOLUTION_OUTPUT_SHAFT * 
         //                                          REVOLUTION_HOOD_PER_REVOLUTION_GEAR *
         //                                          REVOLUTION_HOOD_PER_DEGREES);
+        }   
 
+        // Sets the P to 0.1
+        hoodPID.setP(0.1);
+        // Sets the I to 0
+        hoodPID.setI(0d);
+        // Sets the D to 0
+        hoodPID.setD(0d);
+        // Sets the output range (the min ad max the PID is allowed to go) to -1.5, 1.5 V
+        hoodPID.setOutputRange(-1.5, 1.5);
+                                        
+        // Sets the position to zero on construct
         hoodEncoder.setPosition(0);
 
 
@@ -69,11 +94,14 @@ public class HoodSubsystem extends SubsystemBase{
     public void periodic(){
         SmartDashboard.putNumber("Encoder Position", hoodEncoder.getPosition());
         SmartDashboard.putNumber("Hood Position", hoodPos);
-        // SmartDashboard.putNumber("PID CALCULATE", pidCalc);
 
-        TrapezoidProfile profile = new TrapezoidProfile(constraints, goal, setpoint);
+        // Creates a Trapezoid profile with the given constraints, goal state
+        TrapezoidProfile profile = new TrapezoidProfile(constraints, goal);
+        // Samples the newly created profile 20ms in the future and saves it in a state
         setpoint = profile.calculate(0.02);
-        hoodPID.setReference(setpoint.position, ControlType.kPosition);
+
+        // Sets the setpoint of the PID as the future setpoint
+        hoodPID.setReference(setpoint.position * MOTOR_TICKS_TO_HOOD_DEGREES, ControlType.kPosition);
 
     }
 
@@ -82,23 +110,21 @@ public class HoodSubsystem extends SubsystemBase{
     }
 
     public void moveHoodUp(){
-
-        if (hoodPos < 5){ //8.96
-            hoodPos += 1;
-            goal = new TrapezoidProfile.State(hoodPos, 0);
+        // Checks if the current degrees is less than the max degrees
+        if (hoodEncoder.getPosition() * HOOD_DEGREES_TO_MOTOR_TICKS < MAX_DEGREES_HOOD){
+            goal = new TrapezoidProfile.State((hoodEncoder.getPosition() * HOOD_DEGREES_TO_MOTOR_TICKS) + HOOD_INCREMENT, 0);
         }
     }
 
     public void moveHoodDown(){
-        if (hoodPos > 0){
-            hoodPos -= 1;
-            goal = new TrapezoidProfile.State(hoodPos, 0);
+        // Checks if the current degrees is more than the min degrees
+        if (hoodEncoder.getPosition() * HOOD_DEGREES_TO_MOTOR_TICKS > MIN_DEGREES_HOOD){
+            goal = new TrapezoidProfile.State((hoodEncoder.getPosition() * HOOD_DEGREES_TO_MOTOR_TICKS) - HOOD_INCREMENT, 0);
         }
     }
  
 
     public void zeroHood(){
         hoodPos = 0;
-    }
-    
+    }   
 }
